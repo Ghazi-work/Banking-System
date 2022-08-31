385f6cbe97e7c7af6bf561efebcd47f32a93993e
Syed Ghazi Ali Zaidi <94217827+Ghazi-work@users.noreply.github.com>
2022-08-31 13:10:15 +0500

    Application Initial commit

---
+++ Banking System/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Banking_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
